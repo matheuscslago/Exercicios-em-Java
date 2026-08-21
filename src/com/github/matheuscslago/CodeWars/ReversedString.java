8c82a15d63af43ce1947df342c8d5d158e75de13
Matheus Lago <matheuscarloslago2007@gmail.com>
2026-08-21 09:33:21 -0300

    fiz um exercicio do CodeWars "Reversed String"

---
+++ src/com/github/matheuscslago/CodeWars/ReversedString.java
@@ -0,0 +1,17 @@
+package com.github.matheuscslago.CodeWars;
+
+public class ReversedString {
+    public static String solution(String str) {
+        char[] reversedStr = new char[str.length()];
+        char[] normalStr = str.toCharArray();
+        int i = 0;
+
+        for(char letter : normalStr){
+            letter = normalStr[str.length() - 1 - i];
+            reversedStr[i] = letter;
+            i++;
+        }
+
+        return new String(reversedStr);
+    }
+}
